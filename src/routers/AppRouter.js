b0cf441f819267d320306d1b44ff7376f983361a
Jose Trujillo <joseantoniotsandoval@gmail.com>
2020-11-15 22:06:00 -0300

    second commit

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,31 +1,35 @@
 import React from 'react'
+import { NavBar } from '../components/NavBar'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Redirect
   } from "react-router-dom";
 
-import { LoginScreen } from '../components/LoginScreen';
-
-import { DashboardRoutes } from './DashboardRoutes';
+import { MarvelScreen } from '../components/MarvelScreen'
+import { HeroeScreen } from '../components/HeroeScreen'
+import { DcScreen } from '../components/DcScreen'
 
 export const AppRouter = () => {
     return (
         <Router>
-      <div>
-        
-        
 
-        
-        <Switch>
-          <Route exact path="/login" component={ LoginScreen }/>
-            
-            <Route  path="/" component={ DashboardRoutes }/>
-            
-          
-          
-        </Switch>
+        <NavBar />   
+
+      <div div className="container mt-4">
+
+      <Switch>
+                <Route exact path="/marvel" component={ MarvelScreen }/>
+                <Route exact path="/hero/:heroeId" component={ HeroeScreen }/> { /* aqui se mostraran los 2 tipos de heroes, tanto de marvel como de dc por el publisher.*/} 
+                <Route exact path="/dc" component={ DcScreen }/>
+
+                <Redirect to="/marvel"/>
+
+
+
+
+            </Switch>
       </div>
     </Router>
        
